7eeb2c65bd5f19f0f2198af7240be1e702ad8fe6
Kori <114691978+Kori27@users.noreply.github.com>
2024-03-06 18:22:38 +0100

    Delete mails directory

--- mails/resetPassword.js
+++
@@ -1,7 +0,0 @@
-const resetPasswordMail = (name, link) =>{
-    return `<h1>Kedves ${name}</h1>
-    <p>A jelszava megváltoztatásához kattintson az alábbi linkre: </p>
-    <a href="${link}">${link}</a>`
-}
-
-module.exports = {resetPasswordMail}
--- mails/welcome.js
+++
@@ -1,7 +0,0 @@
-const welcomeMail = (name) =>{
-    return `<h1>Kedves ${name}</h1>
-    <p>Köszönjük hogy regisztrált az oldalunkra</p>
-    <p>Böngésszen ajánlataink között</p>`
-}
-
-module.exports = {welcomeMail}
